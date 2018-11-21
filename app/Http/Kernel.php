1859c5865e58dc3616a726075a7bb123f1ccfdaf
whoisbear <331920955@qq.com>
2018-11-21 16:29:50 +0800

    提交新版

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\Testcheck;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -58,6 +57,5 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'testcheck' =>  \App\Http\Middleware\Testcheck::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,5 @@
 */
 
 Route::get('/', function () {
-//    return "123";
-    //echo date("Y-m-d H:i:s");
-    //Config::set("app.locale","cn");
-    //echo Config::get("app.locale");
-   // echo env("DB_USERNAME");
-    return "welcome";
+    return view('welcome');
 });
-Route::get('/login',function(){
-   return '这是登录页';
-});
-Route::get('/testcheck',function(){
-    return '也是验证通过页';
-})->middleware('testcheck');
-Route::get('/test/index','TestController@index')->name('aaa');
-Route::get('/test/namess','TestController@namess');
-Route::get('/article/index','ArticleController@index');
